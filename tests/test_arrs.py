c0e0d7045930f6dd4e7c628e26b0222d51e6ac93
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-05-10 16:14:22 -0400

    Corrected func "test_get"

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,10 +1,18 @@
-from utils import arrs
 import pytest
+
+from utils import arrs
+
 
 def test_get():
     with pytest.raises(IndexError):
         assert arrs.get([1, 2, 3], 1, "test") == 2
+        assert arrs.get([1, 2, 3], 0, "test") == 1
         assert arrs.get([], 0, "test") == "test"
+        assert arrs.get([1, 2, 3], -1, "test") == []
+        assert arrs.get([1, 4, 3], '1', "test") == 4
+        assert arrs.get(['testing', 'test', 3], 1, "test") == 'test'
+        assert arrs.get('test', 5, 'testing') == 'Данные должны быть в формате списка'
+        assert arrs.get([1, '2', 3], '1', "test") == 'индкес должен быть целым числом'
 
 
 def test_slice():
